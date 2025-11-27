f446d0677dcbf08c5da026f40b9586d63afb8862
Leonardo Alípio <leo.alipio@live.com>
2025-11-27 16:28:23 -0300

    endpoint user register

--- server/main.go
+++ server/main.go
@@ -14,6 +14,7 @@ func main() {
 	router.GET("/movies", controllers.GetMovies())
 	router.GET("/movie/:imdb_id", controllers.GetMovie())
 	router.POST("/addmovie", controllers.AddMovie())
+	router.POST("/register", controllers.RegisterUser())
 
 	fmt.Println("Server is running...")
 	router.Run(":8080")
